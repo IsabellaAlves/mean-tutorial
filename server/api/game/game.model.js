a0b6d80caf63732c74abbf9a0593804337121f6f
unknown <isabellachoucatealves@hotmail.com>
2016-01-26 13:43:13 +1100

    create game schema, seed db

--- server/api/game/game.model.js
+++ server/api/game/game.model.js
@@ -1,11 +1,24 @@
 'use strict';
 
-var mongoose = require('bluebird').promisifyAll(require('mongoose'));
+// var mongoose = require('bluebird').promisifyAll(require('mongoose'));
 
-var GameSchema = new mongoose.Schema({
+// var GameSchema = new mongoose.Schema({
+//   name: String,
+//   info: String,
+//   active: Boolean
+// });
+
+// export default mongoose.model('Game', GameSchema);
+
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+ 
+var GameSchema = new Schema({
   name: String,
-  info: String,
-  active: Boolean
+  platform: String,
+  genre: String
 });
+ 
+module.exports = mongoose.model('Game', GameSchema);
 
-export default mongoose.model('Game', GameSchema);
